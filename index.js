d5e8411e96f8b574e1fe6fcf9cdde9deacf52bb3
Christian Lloyd Arieta <christianlloyd.arieta@edsci.edu.ph>
2023-03-30 15:52:29 +0800

    updated my capstone

--- index.js
+++
@@ -1,6 +0,0 @@
-const email = document.querySelector("#email");
-const formSubmit = document.querySelector("#form-submit");
-
-formSubmit.addEventListener("click", function() {
-	alert("I appreciate the email you sent " + email.value + " regarding to your question. Have a wonderful day!")
-})
